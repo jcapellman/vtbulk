fbbb48463a6250a190bf3e120cf57e7295736b50
Jarred Capellman <jcapellman@hotmail.com>
2020-10-18 12:22:35 -0500

    Added: TextFile implementation

---
+++ InterfaceImplementations/HashList/TextFile.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+using vtbulk.Interfaces;
+
+namespace vtbulk.InterfaceImplementations.HashList
+{
+    public class TextFile : IHashList
+    {
+        public string[] GetHashes(string argument)
+        {
+            if (!File.Exists(argument))
+            {
+                throw new FileNotFoundException($"HASH File does not exist: {argument}");
+            }
+
+            return File.ReadAllLines(argument);
+        }
+
+        public string Name => "TextFile";
+
+        public bool ArgumentRequired => true;
+    }
+}

5fa388591463dce18d84ce9613ab13f0c48f61d4
Jarred Capellman <jcapellman@hotmail.com>
2020-10-18 12:14:38 -0500

    Added: Initial work on the IHashList Interface

---
+++ Interfaces/IHashList.cs
@@ -0,0 +1,11 @@
+﻿namespace vtbulk.Interfaces
+{
+    public interface IHashList
+    {
+        string Name { get; }
+
+        bool ArgumentRequired { get; }
+
+        string[] GetHashes(string argument);
+    }
+}

e14ef70986e2f85771a3b5336e898066f43ab584
Jarred Capellman <jcapellman@hotmail.com>
2020-10-18 12:19:57 -0500

    Added: HashSource and HashListSourceArgument properties to the CommandLineArgumentsItem class

--- Objects/CommandLineArgumentsItem.cs
+++ Objects/CommandLineArgumentsItem.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 
+using vtbulk.Interfaces;
+
 namespace vtbulk.Objects
 {
     public class CommandLineArgumentsItem
     {
         public string OutputFilePath { get; set; }
 
-        public string InputHashFile { get; set; }
-    
         public string VTKey { get; set; }
 
         public bool VerboseOutput { get; set; }
 
         public bool EnableMultithreading { get; set; }
+
+        public IHashList HashSource { get; set; }
+
+        public string HashListSourceArgument { get; set; }
 
         public CommandLineArgumentsItem()
         {
